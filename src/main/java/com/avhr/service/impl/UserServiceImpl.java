288f1b47ff46d92815c60f1f698c3491dfe45d50
JimmyLin <770644125@qq.com>
2016-07-23 01:34:33 +0800

    解决了key的生成问题

--- src/main/java/com/avhr/service/impl/UserServiceImpl.java
+++ src/main/java/com/avhr/service/impl/UserServiceImpl.java
@@ -3,7 +3,7 @@ package com.avhr.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.avhr.dao.UserDao;
+import com.avhr.dao.UserMapper;
 import com.avhr.model.User;
 import com.avhr.service.UserService;
 
@@ -11,10 +11,10 @@ import com.avhr.service.UserService;
 public class UserServiceImpl implements UserService {
 
 	@Autowired
-	UserDao userDao;
+	UserMapper userMapper;
 	public int insertUser(User user) {
 		// TODO Auto-generated method stub
-		return userDao.insert(user);
+		return userMapper.insert(user);
 	}
 
 }
--- src/test/java/com/avhr/test/UserServiceTest.java
+++ src/test/java/com/avhr/test/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.avhr.test;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -24,9 +25,10 @@ public class UserServiceTest {
 	public void testInsertUser() {
 		User user =new User();
 		user.setAge("21");
-		user.setName("aaa");
+		user.setName(System.currentTimeMillis()+"");
 		user.setPassword("111");
-		//System.out.println(userService.insertUser(user));
+		System.out.println(userService.insertUser(user));
+		System.out.println(user.getId());
 	}
 
 }
